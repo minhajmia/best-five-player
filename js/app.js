cd8dbfd0a9cfb1b463e3c45cfedee36fd98ec7ad
minhajmia <minhajmia418@gmail.com>
2022-08-23 19:23:29 +0600

    display player 5

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+const playerList = [];
+
+function displayPlayer(players) {
+  const tbody = document.getElementById("player-list");
+  tbody.innerHTML = " ";
+  for (let i = 0; i < players.length; i++) {
+    const name = players[i].playerName;
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${i + 1}</td>
+    <td>${name}</td>
+    `;
+    tbody.appendChild(tr);
+  }
+}
+
 function addToCart(player) {
   const name = player.parentNode.children[0].innerText;
   const playerObj = {
